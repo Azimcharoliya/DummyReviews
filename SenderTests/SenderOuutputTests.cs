2d656e4f8efa295f241c2ec28e5c51e4974ebca3
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 13:38:13 +0530

    update test cases

--- SenderTests/SenderOuutputTests.cs
+++ SenderTests/SenderOuutputTests.cs
@@ -1,15 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Xunit;
-using Sender;
 namespace SenderTests
 {
-    class MockConsoleOuput : ISenderOutput
+    public class MockConsoleOutput : ISenderOutput
     {
         internal List<List<String>> OutputOnConsole = new List<List<string>>();
-
+        internal int n_rows, n_columns;
         public void WriteOutput(IEnumerable<IEnumerable<String>> data)
         {
+            n_rows = ConsoleOutput.GetNumberofRows(data);
+            n_columns = ConsoleOutput.GetNumberofColumns(data);
             List<String> newRow;
             foreach (IEnumerable<String> row in data)
             {
@@ -26,7 +27,7 @@ namespace SenderTests
     public class SenderOutputTests
     {
         [Fact]
-        public void WhenCalledWithTwoDimensionalDataThenAccessValuesRowWiseFromData()
+        public void WhenCalledWithTwoDimensionalIEnumerableThenGiveProperRowAndColumnCountAndAccessValuesRowWiseFromData()
         {
             List<List<String>> testinput = new List<List<string>>
             {
@@ -36,10 +37,12 @@ namespace SenderTests
                 new List<string> { "30/11/2015", "Edge Cases not handled" }
             };
 
-            MockConsoleOuput mockConsoleOuput = new MockConsoleOuput();
+            MockConsoleOutput mockConsoleOuput = new MockConsoleOutput();
             mockConsoleOuput.WriteOutput(testinput);
 
             List<List<String>> testoutput = mockConsoleOuput.OutputOnConsole;
+            Assert.Equal(4, mockConsoleOuput.n_rows);
+            Assert.Equal(2, mockConsoleOuput.n_columns);
             Assert.Equal(testinput, testoutput);
         }
 
@@ -57,6 +60,21 @@ namespace SenderTests
             int noofColumns = ConsoleOutput.GetNumberofColumns(testinput);
             Assert.True(noofColumns == 2);
         }
+        [Fact]
+        public void WhenCalledWithTwoDimensionalDataThenReturnNumberofRowsinData()
+        {
+            List<List<String>> testinput = new List<List<string>>
+            {
+                new List<string> { "Date", "Comment" },
+                new List<string> { "12/12/2012", "All good" },
+                new List<string> { "11/11/2011", "Remove duplication" },
+                new List<string> { "30/11/2015", "Edge Cases not handled" }
+            };
+
+            int n_rows = ConsoleOutput.GetNumberofRows(testinput);
+            Assert.True(n_rows == 4);
+        }
 
     }
 }
+
