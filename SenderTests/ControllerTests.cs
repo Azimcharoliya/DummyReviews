a8658c609edcab0560e38c12426abaf3cde59271
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 13:54:51 +0530

    update to add Main

--- SenderTests/ControllerTests.cs
+++ SenderTests/ControllerTests.cs
@@ -37,5 +37,8 @@ namespace SenderTests
             controller.WriteOutput(parsedinput);
             Assert.Equal("sampledata", consoleOutput.OutputOnConsole[0][0]);
         }
+        static void Main()
+        {
+        }
     }
 }
