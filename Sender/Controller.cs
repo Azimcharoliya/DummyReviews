e36aea22659d2e5e5f98ed4ce40374cae16a458a
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 13:34:34 +0530

    update controller

--- Sender/Controller.cs
+++ Sender/Controller.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Sender
@@ -14,6 +14,7 @@ namespace Sender
         }
         public IEnumerable<IEnumerable<string>> ReadInput()
         {
+            inputInterface.InputExceptionHandler();
             return inputInterface.ReadInput();
         }
 
@@ -21,16 +22,18 @@ namespace Sender
         {
             outputInterface.WriteOutput(parsedData);
         }
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
-            string filepath = @"E:\BootCamp\ReviewOfReviews\Sender\Comments.csv";
+            string filepath = @"E:\BootCamp\Sender\SenderTests\EmptySample.csv";
             //string filter = args[0];
             //Console.WriteLine(filter);
             CSVInput csvInput = new CSVInput(filepath);
             ConsoleOutput consoleOutput = new ConsoleOutput();
             Controller controller = new Controller(csvInput, consoleOutput);
             List<List<string>> parsedinput = (List<List<string>>)controller.ReadInput();
+            //Console.WriteLine(parsedinput.Count);
             controller.WriteOutput(parsedinput);
-        }
+        }*/
     }
 }
+
